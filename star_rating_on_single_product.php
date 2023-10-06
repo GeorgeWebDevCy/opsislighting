ea43c0c18cd65ca9558f31eaa37be7bfc0eafc07
George Nicolaou <orionas.elite@gmail.com>
2023-10-06 17:31:19 +0300

    code updated

--- star_rating_on_single_product.php
+++ star_rating_on_single_product.php
@@ -5,20 +5,13 @@ function add_product_reviews_shortcode_after_price() {
     // Check if we are on a single product page
     if (is_product()) {
         echo '<div class="wcpr-overall-rating-main">';
-        echo '<div class="wcpr-overall-rating-left">';
+        echo '<div class="wcpr-overall-rating-right-star">';
         
         $product = wc_get_product(get_the_ID());
         $average_rating = $product->get_average_rating();
         $review_count = $product->get_review_count(); // Get the total number of reviews
         
-        echo '<span class="wcpr-overall-rating-left-average">';
-        echo number_format($average_rating, 2); // Display the average rating with two decimal places
-        echo '</span>';
-        echo '</div>';
-        echo '<div class="wcpr-overall-rating-right">';
-        echo '<div class="wcpr-overall-rating-right-star">';
-        
-        // Generate the star rating HTML based on the average rating or use zero if there are no reviews
+        // Generate the star rating HTML based on the average rating
         $stars_html = ($review_count > 0) ? wc_get_rating_html($average_rating, 5) : wc_get_rating_html(0, 5);
         echo $stars_html;
         
@@ -28,11 +21,10 @@ function add_product_reviews_shortcode_after_price() {
         if ($review_count === 0) {
             echo 'No reviews';
         } else {
-            echo 'Based on ' . $review_count . ' review' . ($review_count === 1 ? '' : 's');
+            echo '(' . $review_count . ' review' . ($review_count === 1 ? ')' : 's)');
         }
         echo '</div>';
         
-        echo '</div>';
         echo '</div>';
     }
 }
